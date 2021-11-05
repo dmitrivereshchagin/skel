44899064cd6245f2e965f77694ce0358e80645d4
Dmitri Vereshchagin <dmitri.vereshchagin@gmail.com>
2021-11-05 12:32:40 +0300

    Move Mutt private configuration and cache away

---
+++ bin/xdg-basedir.sh
@@ -0,0 +1,14 @@
+__xdg_path() {
+    case $(eval echo "\${$1-}") in
+    /*) eval echo "\$$1" ;;
+    *)  echo "$2"
+    esac
+}
+
+xdg_config_home() {
+    __xdg_path XDG_CONFIG_HOME "$HOME/.config"
+}
+
+xdg_cache_home() {
+    __xdg_path XDG_CACHE_HOME "$HOME/.cache"
+}
