743c8796f3245486fc304a17966ef3c174f4c32c
Dmitri Vereshchagin <dmitri.vereshchagin@gmail.com>
2022-01-04 13:18:28 +0300

    Hide xsel log file

--- bin/xdg-basedir.sh
+++ bin/xdg-basedir.sh
@@ -9,6 +9,10 @@ xdg_config_home() {
     __xdg_path XDG_CONFIG_HOME "$HOME/.config"
 }
 
+xdg_state_home() {
+    __xdg_path XDG_STATE_HOME "$HOME/.local/state"
+}
+
 xdg_cache_home() {
     __xdg_path XDG_CACHE_HOME "$HOME/.cache"
 }
