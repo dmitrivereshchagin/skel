d5fbfdc10ceea52fc28b2009198002563b52d6d0
Dmitri Vereshchagin <dmitri.vereshchagin@gmail.com>
2024-02-13 22:36:50 +0300

    Make bin/xdg.sh easier to use

--- bin/xdg.sh
+++ bin/xdg.sh
@@ -1,18 +1,13 @@
-__xdg_path() {
-    case $(eval echo "\$$1") in
-    /*) eval echo "\$$1" ;;
-    *)  echo "$2"
-    esac
-}
+# shellcheck shell=sh
 
-xdg_config_home() {
-    __xdg_path XDG_CONFIG_HOME "$HOME/.config"
-}
+case ${XDG_CONFIG_HOME-} in
+''|[!/]*) XDG_CONFIG_HOME=$HOME/.config
+esac
 
-xdg_state_home() {
-    __xdg_path XDG_STATE_HOME "$HOME/.local/state"
-}
+case ${XDG_STATE_HOME-} in
+''|[!/]*) XDG_STATE_HOME=$HOME/.local/state
+esac
 
-xdg_cache_home() {
-    __xdg_path XDG_CACHE_HOME "$HOME/.cache"
-}
+case ${XDG_CACHE_HOME-} in
+''|[!/]*) XDG_CACHE_HOME=$HOME/.cache
+esac
